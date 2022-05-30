2fadbd109923ff94e5ddb04b0df0d33decc6941c
ChaseYin <yxf993993@yahoo.com>
2022-05-30 17:02:03 +0800

    none

--- test.js
+++ test.js
@@ -1,8 +1,6 @@
 let a = 1;
 console.log(a);
 
-
-
 //测试本地多个分支同时开发
 let b = 3;
 console.log(b);
@@ -10,4 +8,9 @@ console.log(b);
 var c = 4;
 console.log(4);
 
+let aaa = {
+  a: 123,
+  b: 456,
+  c: { a: 123 },
+};
 //branch2
